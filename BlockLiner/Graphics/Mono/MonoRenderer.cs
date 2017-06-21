667a998b8def3622963b4c1b5d0124adda0144ab
Jiksaa <jonathan.beersaerts1993@gmail.com>
2017-06-21 21:57:26 +0200

    Adding vectors computation in MonoRender
    To ease drawing, each position is compute to generate two array of Vector2.
    
    One array for border block position and an other for gameAreaBlockPosition
    
    The second one, is a two-dimentionnal array representing the the gamearea matrix

--- BlockLiner/Graphics/Mono/MonoRenderer.cs
+++ BlockLiner/Graphics/Mono/MonoRenderer.cs
@@ -17,15 +17,28 @@ namespace BlockLiner.Graphics.Mono
         private Texture2D _borderTexture;
         private Texture2D _blockTexture;
 
+        private Vector2[] _borderpPositionVectors;
+        private Vector2[,] _gameAreaPositonVectors;
+
         public MonoRenderer(GraphicsDevice graphics, uint width, uint height)
         {
             _witdh = width;
             _height = height;
             _graphicDevice = graphics;
             _spritebatch = new SpriteBatch(_graphicDevice);
+            
+            // instantiate Vector2 arrays
+
+            // borderPosition is 2 vertical line of _height blocks in addition
+            // to the horizontal border of _width blocks
+            _borderpPositionVectors = new Vector2[_height * 2 + _witdh];
+            // replicate the size of the gameArea
+            _gameAreaPositonVectors = new Vector2[_witdh, _height];
 
             InitializeTextures();
 
+            BuildBorderPositionVector();
+            BuildGameAreaPositionVector();
         }
 
         private void InitializeTextures()
@@ -49,7 +62,53 @@ namespace BlockLiner.Graphics.Mono
 
         private void BuildBorderPositionVector()
         {
+            int xOffset = _BLOCKSIZE;
+            int yOffset = _BLOCKSIZE;
 
+            //vline vectors construction
+            int arrayIndex = 0;
+            for (int i = 0; i < _height; i++)
+            {
+                // compute actual x y position
+                // offset + block line number * blocksize
+                int currentY = yOffset + i * _BLOCKSIZE;
+                int currentX = xOffset;
+
+                // left vline border
+                _borderpPositionVectors[arrayIndex] = new Vector2(currentX, currentY);
+                arrayIndex++;
+
+                // right vline border
+                currentX += _BLOCKSIZE + ((int)_witdh + 1) * _BLOCKSIZE;
+                _borderpPositionVectors[arrayIndex] = new Vector2(currentX, currentY);
+                arrayIndex++;
+            }
+
+            //hline vectors construction
+            int hlineY = yOffset + (int)_height * _BLOCKSIZE;
+            for (int i = 0; i < _witdh; i++)
+            {
+                int hlineX = xOffset + (i + 1) * _BLOCKSIZE;
+                _borderpPositionVectors[arrayIndex] = new Vector2(hlineX, hlineY);
+                arrayIndex++;
+            }
+
+        }
+
+        private void BuildGameAreaPositionVector()
+        {
+            int xOffset = _BLOCKSIZE * 2;
+            int yOffset = _BLOCKSIZE;
+
+            for (int x = 0; x < _witdh; x++)
+            {
+                int currentX = xOffset + x * _BLOCKSIZE;
+                for (int y = 0; y < _height; y++)
+                {
+                    int currentY = yOffset + y * _BLOCKSIZE;
+                    _gameAreaPositonVectors[x, y] = new Vector2(currentX, currentY);
+                }
+            }
         }
 
         public void Begin()
